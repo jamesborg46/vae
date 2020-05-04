d145222cbc438c45cb623fa821c64a767419cb97
James <borg@mi.t.u-tokyo.ac.jp>
2020-05-04 20:04:11 +0900

    EXP_18

--- run.sh
+++ run.sh
@@ -1,16 +1,16 @@
 python ./vae.py \
-    --name EXP_17 \
+    --name EXP_18 \
     --batch-size 100 \
     --test-batch-size 1000 \
     --epochs 500 \
     --lr 0.01 \
     --encoder-units 500 \
     --decoder-units 500 \
-    --decoder-weight-decay 0.0 \
+    --decoder-weight-decay 0.1 \
     --latent-dim 16 \
     --custom-init \
     --weight-init-std 0.01 \
-    --bias-init-std 0.0 \
-    --z-std-prior 1.0 \
+    --bias-init-std 0.01 \
+    --z-std-prior 0.1 \
     --pre-normalization \
 
