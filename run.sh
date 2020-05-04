2127d774859124c6bd7f0bb7505dad2bfe93ff43
James <borg@mi.t.u-tokyo.ac.jp>
2020-05-04 17:05:02 +0900

    EXP_11

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 python ./vae.py \
-    --name EXP_10 \
+    --name EXP_11 \
     --batch-size 128 \
     --test-batch-size 1000 \
     --epochs 500 \
-    --lr 0.003 \
+    --lr 0.0003 \
     --encoder-units 500 \
     --decoder-units 500 \
     --decoder-weight-decay 0.01 \
