d0e4cd487970e0c38a18730385363e69872728d6
James <borg@mi.t.u-tokyo.ac.jp>
2020-05-04 17:03:27 +0900

    EXP_10

--- run.sh
+++ run.sh
@@ -1,15 +1,15 @@
 python ./vae.py \
-    --name EXP_9 \
+    --name EXP_10 \
     --batch-size 128 \
     --test-batch-size 1000 \
     --epochs 500 \
     --lr 0.003 \
     --encoder-units 500 \
     --decoder-units 500 \
-    --decoder-weight-decay 0.1 \
+    --decoder-weight-decay 0.01 \
     --latent-dim 8 \
-    --weight-init-std 0.1 \
+    --weight-init-std 0.001 \
     --bias-init-std 0.0 \
-    --z-std-prior 1 \
+    --z-std-prior 0.1 \
     --sigmoidal-mean
 
