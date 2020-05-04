68981b4031d19ffdfa9474cb7ffb2b285e9977ba
James <borg@mi.t.u-tokyo.ac.jp>
2020-05-04 16:19:53 +0900

    EXP_7

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 python ./vae.py \
-    --name EXP_6_NO_NORM_SIGM_WTIH_VISUALS \
+    --name EXP_7_SMALL_LR_NO_NORM_SIGM_WTIH_VISUALS \
     --batch-size 128 \
     --test-batch-size 1000 \
     --epochs 500 \
-    --lr 0.001 \
+    --lr 0.00001 \
     --encoder-units 500 \
     --decoder-units 500 \
     --decoder-weight-decay 0.5 \
