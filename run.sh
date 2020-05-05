52e6fc8b2b23c95ef22166d9beb93ae11a3e4cd3
James <borg@mi.t.u-tokyo.ac.jp>
2020-05-05 16:35:17 +0900

    EXP_28: largening init and loosening weight decay

--- run.sh
+++ run.sh
@@ -1,16 +1,16 @@
 python ./vae.py \
-    --name EXP_27 \
+    --name EXP_28 \
     --batch-size 100 \
     --test-batch-size 1000 \
     --epochs 500 \
-    --lr 0.01 \
+    --lr 0.0001 \
     --encoder-units 500 \
     --decoder-units 500 \
-    --decoder-weight-decay 0.1 \
+    --decoder-weight-decay 0.01 \
     --latent-dim 16 \
     --custom-init \
-    --weight-init-std 0.01 \
-    --bias-init-std 0.01 \
+    --weight-init-std 0.1 \
+    --bias-init-std 0.1 \
     --decoder-type "gaussian" \
     --sigmoidal-mean \
     --z-std-prior 1 \
--- vae.py
+++ vae.py
@@ -58,7 +58,7 @@ class GaussianDecoder(nn.Module):
         mean = self.mean(z)
         if self.sigmoidal_mean:
             mean = torch.sigmoid(mean)
-        log_var = self.log_var(z) - 5
+        log_var = self.log_var(z) - 7
         var = torch.exp(log_var)
         std = torch.sqrt(var)
         return mean, std
